f3f0a29c06b75edcc01e8c155007e38526f9ee35
jacky6024 <jacky6024@sina.com>
2016-09-05 21:31:10 +0800

    new update in HOME

--- src/MsgBox.js
+++ src/MsgBox.js
@@ -75,5 +75,20 @@ function buildDialog(title,dialogContent,buttons,large){
         let okBtn=$(`<button type="button" class="btn btn-default" data-dismiss="modal">确定</button>`);
         footer.append(okBtn);
     }
+
+    modal.on("show.bs.modal", function() {
+        var index=1050;
+        $(document).find('.modal').each(function (i,d) {
+            var zIndex=$(d).css('z-index');
+            if(zIndex && zIndex!=='' && !isNaN(zIndex)){
+                zIndex=parseInt(zIndex);
+                if(zIndex>index){
+                    index=zIndex;
+                }
+            }
+        });
+        index++;
+        modal.css({'z-index':index});
+    });
     return modal;
 };
